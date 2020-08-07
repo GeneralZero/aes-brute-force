a1715a7cf970776cf0713193ba1726c259dbca44
Brian Ridings <bdrid001@gmail.com>
2020-08-07 17:42:34 -0400

    Delete build_test.sh

--- build_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-baseDir=.
-srcDir=$baseDir/src
-includeDir=$baseDir/include
-
-if [ -z ${CXX+x} ]; then CXX=c++; fi
-
-$CXX -Ofast -Wall -march=native -std=c++11 -I ./include $srcDir/main.cpp -o aes-brute-force -lpthread $*
